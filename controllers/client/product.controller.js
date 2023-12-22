ad6853cb8670b82290c9e73e021b7760d4d4107f
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2023-12-22 22:00:38 +0700

    Cấu trúc lại folder PUG

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -3,7 +3,9 @@
 
 // index là tên hàm tự đặt, t/ư vs phương thức [GET] /products/
 module.exports.index = (req, res) => {
-  res.render("client/pages/products/index.pug");
+  res.render("client/pages/products/index.pug", {
+    pageTitle: "Trang danh sách sản phẩm"
+  });
 }
 
 // [GET] /products/detail
