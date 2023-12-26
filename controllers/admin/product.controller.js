b4dcbd1b74c4012b2255964ba36fc4d7785d448e
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2023-12-26 21:43:35 +0700

    Lấy data sản phẩm in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,12 @@
+const Product = require("../../models/product.model.js")
 // [GET] /admin/product/
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    delete: false
+  });
+
   res.render("admin/pages/products/index.pug", {
-    pageTitle: "Trang danh sách sản phẩm"
+    pageTitle: "Trang danh sách sản phẩm",
+    products: products
   });
 }
