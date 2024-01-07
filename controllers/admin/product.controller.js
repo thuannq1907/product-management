cd0bef1caca65de82d555f47609dc0e289e7d8f7
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2024-01-08 06:06:10 +0700

    Hiển thị thông báo sau khi đổi trạng thái p2

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -102,7 +102,10 @@ module.exports.changeMulti = async (req, res) => {
       } , {
         status: type
       });
+
+      req.flash('success', 'Cập nhật trạng thái thành công');
       break;
+
     case "delete-all":
       await Product.updateMany({
           _id: { $in: ids }
@@ -110,7 +113,10 @@ module.exports.changeMulti = async (req, res) => {
           deleted: true,
           deletedAt: new Date()
         });
+
+        req.flash('success', 'Xóa sản phẩm thành công');
       break;
+
     case "change-position":
       for (const item of ids) {
         // split("-") tách id và position ra thành 1 mảng
@@ -125,12 +131,14 @@ module.exports.changeMulti = async (req, res) => {
           position: position
         })
       }
+
+      req.flash('success', 'Thay đổi vị trí thành công');
       break;
+
     default:
       break;
   }
 
-  req.flash('success', 'Cập nhật trạng thái thành công');
 
   res.redirect("back");
 }
@@ -152,6 +160,8 @@ module.exports.deleteItem = async (req, res) => {
       deleted: true,
       deletedAt: new Date()
     });
+
+    req.flash('success', 'Xóa sản phẩm thành công');
   } catch (error) {
     console.log(error);
   }
