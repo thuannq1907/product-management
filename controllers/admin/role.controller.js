94164bc65caa949fa95524eb967cf515ae3fe84b
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2024-01-20 10:12:53 +0700

    Xây dựng phần Thêm mới nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,6 +1,29 @@
+const Role = require("../../models/role.model.js");
+
+const systemConfig = require("../../config/system");
+
 // [GET] /admin/roles/
-module.exports.index = (req, res) => {
-  res.render("admin/pages/roles/index.pug", {
-    pageTitle: "Nhóm quyền"
+module.exports.index = async (req, res) => {
+  const records = await Role.find({
+    deleted: false
   });
+  res.render("admin/pages/roles/index.pug", {
+    pageTitle: "Nhóm quyền",
+    records: records
+  });
+}
+
+// [GET] /admin/roles/create
+module.exports.create = (req, res) => {
+  res.render("admin/pages/roles/create.pug", {
+    pageTitle: "Thêm mới Nhóm quyền"
+  });
+}
+
+// [POST] /admin/roles/create
+module.exports.createPost = async (req, res) => {
+  const record = new Role(req.body);
+  await record.save();
+
+  res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const controller = require("../../controllers/admin/role.controller.js");
 
-// / <=> /admin/roles/
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post("/create", controller.createPost);
 
 module.exports = router;
