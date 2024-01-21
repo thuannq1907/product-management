fc06a7a854a12ef8cf9ea5b3dab1196c4061d69c
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2024-01-21 22:23:36 +0700

    Làm trang tạo tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,4 +1,10 @@
+const md5 = require('md5');
 const Account = require("../../models/account.model.js");
+const Role = require("../../models/role.model");
+
+const generateHelper = require("../../helpers/generate.helper");
+
+const systemConfig = require("../../config/system");
 
 // [GET] /admin/accounts/
 module.exports.index = async (req, res) => {
@@ -14,4 +20,30 @@ module.exports.index = async (req, res) => {
     pageTitle: "Danh sách tài khoản",
     records: records,
   });
+};
+
+// [GET] /admin/accounts/create
+module.exports.create = async (req, res) => {
+  const roles = await Role.find({
+    deleted: false
+  });
+
+  console.log(roles);
+
+  res.render("admin/pages/accounts/create.pug", {
+    pageTitle: "Tạo mới tài khoản",
+    roles: roles
+  });
+};
+
+// [POST] /admin/accounts/create
+module.exports.createPost = async (req, res) => {
+
+  req.body.token = generateHelper.generateRandomString(30);
+  req.body.password = md5(req.body.password);
+
+  const record = new Account(req.body);
+  await record.save();
+
+  res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
 };
