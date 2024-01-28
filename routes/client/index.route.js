61dd9e0dee18c1e92dabe8d966876e3294a049e0
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2024-01-28 21:15:27 +0700

    Hiển thị menu chung bên client (Lấy data in ra giao diện)

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,13 @@
 const productRoutes = require("./product.route.js")
 const homeRoutes = require("./home.route.js")
 
+const categoryMiddleware = require("../../middlewares/client/category.middleware")
+
 module.exports = (app) => {
-  app.get("/", homeRoutes);
+  // Vì cái danh mục sản phẩm này k cần bảo mật như authMiddleware nên có thể sd luôn
+  // app.use(categoryMiddleware.category);
+
+  app.get("/", categoryMiddleware.category, homeRoutes);
   
-  app.use("/products", productRoutes);
+  app.use("/products", categoryMiddleware.category, productRoutes);
 }
