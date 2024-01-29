2f495698ef61eb90fa9259ad473101c47b529b7a
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2024-01-30 06:32:14 +0700

    Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,6 +2,7 @@ const Product = require("../../models/product.model");
 
 // [GET] /
 module.exports.index = async (req, res) => {
+  // Sản phẩm nổi bật
   const productsFeatured = await Product.find({
     featured: "1",
     status: "active",
@@ -9,11 +10,24 @@ module.exports.index = async (req, res) => {
   }).sort({ position: "desc" }).limit(6);
 
   for (const item of productsFeatured) {
-    item.priceNew = item.price * (100 - item.discountPercentage)/100;
+    item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
   }
+  // Hết Sản phẩm nổi bật
+
+  // Sản phẩm mới nhất
+  const productsNew = await Product.find({
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" }).limit(6);
+
+  for (const item of productsNew) {
+    item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+  }
+  // Hết Sản phẩm mới nhất
 
   res.render("client/pages/home/index.pug", {
     pageTitle: "Trang chủ",
-    productsFeatured: productsFeatured
+    productsFeatured: productsFeatured,
+    productsNew: productsNew
   });
 }
