4c390c96de1db8b6f28b235f9fafae556f1f7717
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2024-01-30 06:23:47 +0700

    Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,19 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+// [GET] /
+module.exports.index = async (req, res) => {
+  const productsFeatured = await Product.find({
+    featured: "1",
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" }).limit(6);
+
+  for (const item of productsFeatured) {
+    item.priceNew = item.price * (100 - item.discountPercentage)/100;
+  }
+
   res.render("client/pages/home/index.pug", {
-    pageTitle: "Trang chủ"
+    pageTitle: "Trang chủ",
+    productsFeatured: productsFeatured
   });
 }
