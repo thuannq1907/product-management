c614fb09f20d315bcd9478214acb91bd90e36b71
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2024-02-03 14:39:39 +0700

    Làm nút Mini Cart trên Header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -2,6 +2,7 @@ const Cart = require("../../models/cart.model");
 
 module.exports.cart = async (req, res, next) => {
   // ktra, nếu chưa có giỏ hàng thì tạo mới giỏ hàng (rỗng) và lưu vào database
+  // TH lần đầu vào trang
   if(!req.cookies.cartId) {
     const cart = new Cart();
     await cart.save();
@@ -11,6 +12,13 @@ module.exports.cart = async (req, res, next) => {
 
     // sau khi tạo mới giỏ hàng rồi thì lưu id của giỏ hàng đó vào cookie
     res.cookie("cartId", cart.id, { expires: new Date(Date.now() + expire) });
+  } else {
+    //TH nếu ngta load lại trang (vào lần t2 trở đi) thì trả về thông tin cái giỏ hàng
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId
+    });
+
+    res.locals.cart = cart;
   }
   next();
 }
