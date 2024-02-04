e529a2f714915a21d24da840f842afebe6d58563
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2024-02-04 09:45:05 +0700

    Lấy data được gửi lên lưu vào database

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,5 +1,6 @@
 const Cart = require("../../models/cart.model");
 const Product = require("../../models/product.model");
+const Order = require("../../models/order.model");
 
 // [GET] /checkout/
 module.exports.index = async (req, res) => {
@@ -31,4 +32,53 @@ module.exports.index = async (req, res) => {
     pageTitle: "Đặt hàng",
     cartDetail: cart
   });
+};
+
+// [POST] /checkout/order
+module.exports.order = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const infoUser = req.body;
+
+  const orderInfo = {
+    cart_id: cartId,
+    userInfo: infoUser,
+    products: [],
+  };
+
+  // có id giỏ hàng rồi -> lấy ra các id sp trong giỏ hàng = cart.products
+  const cart = await Cart.findOne({
+    _id: cartId
+  });
+
+  // có id sp rồi -> lấy chi tiết sp thông qua id: price, %, quantity
+  for (const product  of cart.products) {
+    const infoProduct = await Product.findOne({
+      _id: product.product_id
+    });
+
+    // định nghĩa 1 object có dạng ntn
+    const objectProduct = {
+      product_id: product.product_id,
+      price: infoProduct.price,
+      discountPercentage: infoProduct.discountPercentage,
+      quantity: product.quantity,
+    }
+
+    // push object đó vào mảng products của object orderInfo
+    orderInfo.products.push(objectProduct);
+  }
+
+  // lưu vào database
+  const order = new Order(orderInfo);
+  await order.save();
+
+  // sau khi đặt hàng xong -> update lại giỏ hàng trống, cho products thành 1 mảng rỗng
+  await Cart.updateOne({
+    _id: cartId
+  }, {
+    products: []
+  });
+
+  // chuyển hướng sang trang đặt hàng thành công
+  res.redirect(`/checkout/success/${order.id}`);
 };
