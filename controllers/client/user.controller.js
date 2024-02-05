cac644d9381b73cc806dce68144a6d22aec8ea73
Nguyen Quang Thuan <nguyenquangthuan190820@gmail.com>
2024-02-05 09:46:15 +0700

    Quên mật khẩu - Giao diện nhập email

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,5 +1,6 @@
 const md5 = require("md5");
 const User = require("../../models/user.model");
+const ForgotPassword = require("../../models/forgot-password.model");
 
 const generateHelper = require("../../helpers/generate.helper");
 
@@ -83,4 +84,43 @@ module.exports.loginPost = async (req, res) => {
 module.exports.logout = async (req, res) => {
   res.clearCookie("tokenUser");
   res.redirect("/");
+};
+
+// [GET] /user/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+  res.render("client/pages/user/forgot-password.pug", {
+    pageTitle: "Lấy lại mật khẩu",
+  });
+};
+
+
+// [POST] /user/password/forgot
+module.exports.forgotPasswordPost = async (req, res) => {
+  const email = req.body.email;
+
+	const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+
+  if (!user) {
+    req.flash("error", "Email không tồn tại!");
+    res.redirect("back");
+    return;
+  }
+
+  const otp = generateHelper.generateRandomNumber(5);
+
+  // Việc 1: Lưu thông tin vào database
+  const objectForgotPassword = {
+    email: email,
+    otp: otp
+  };
+
+  const record = new ForgotPassword(objectForgotPassword);
+  await record.save();
+
+  // Việc 2: Gửi mã OTP qua email
+
+  res.redirect(`/user/password/otp?email=${email}`);
 };
